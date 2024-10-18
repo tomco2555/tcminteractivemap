d316c0e584de172708e0dc965973f2a48778748d
Tomáš Šedo <xsedot00@vutbr.cz>
2024-10-18 03:40:40 +0200

    Added toggle switch between DOM and canvas mode

--- static/script.js
+++ static/script.js
@@ -389,3 +389,235 @@ function watchCanvasCheckbox(playlist_name) {
         }
     }
 }
+
+//Hides and shows Motorfest Grounds
+function MFGroundsCanvasCheckbox() {
+
+    var mf_grounds_checkbox_status = document.getElementById("mf_grounds_checkbox").checked;
+
+    if(mf_grounds_checkbox_status) {
+        markers_canvas.removeMarkers(mf_grounds_group);
+        markers_canvas.addMarkers(mf_grounds_group);
+    }
+
+    else if(!mf_grounds_checkbox_status) {
+        markers_canvas.removeMarkers(mf_grounds_group);
+    }
+}
+
+//Hides and shows Demo Royale starting places
+function DemoRoyaleCanvasCheckbox() {
+
+    var demo_royale_checkbox_status = document.getElementById("demo_royale_checkbox").checked;
+
+    if(demo_royale_checkbox_status) {
+        markers_canvas.removeMarkers(demo_royale_group);
+        markers_canvas.addMarkers(demo_royale_group);
+    }
+
+    else if(!demo_royale_checkbox_status) {
+        markers_canvas.removeMarkers(demo_royale_group);
+    }
+}
+
+//Hides and shows Grand Races starting places
+function GrandRaceCanvasCheckbox() {
+
+    var grand_race_checkbox_status = document.getElementById("grand_race_checkbox").checked;
+
+    if(grand_race_checkbox_status) {
+        markers_canvas.removeMarkers(grand_race_group);
+        markers_canvas.addMarkers(grand_race_group);
+    }
+
+    else if(!grand_race_checkbox_status) {
+        markers_canvas.removeMarkers(grand_race_group);
+    }
+}
+
+//Hides and shows Achievement locations
+function AchievementsCanvasCheckbox() {
+
+    var achievements_checkbox_status = document.getElementById("achievements_checkbox").checked;
+
+    if(achievements_checkbox_status) {
+        markers_canvas.removeMarkers(achievements_group);
+        markers_canvas.addMarkers(achievements_group);
+    }
+
+    else if(!achievements_checkbox_status) {
+        markers_canvas.removeMarkers(achievements_group);
+    }
+}
+
+//Hides and shows Treasure locations
+function TreasureCanvasCheckbox() {
+
+    var treasure_checkbox_status = document.getElementById("treasure_checkbox").checked;
+
+    if(treasure_checkbox_status) {
+        markers_canvas.removeMarkers(treasure_group);
+        markers_canvas.addMarkers(treasure_group);
+    }
+
+    else if(!treasure_checkbox_status) {
+        markers_canvas.removeMarkers(treasure_group);
+    }
+}
+
+//Checks toggle switch for performance mode
+document.addEventListener("DOMContentLoaded", function() {
+    const toggle_switch = document.getElementById("toggle_switch");
+    
+    toggle_switch.checked = true;
+})
+
+//Switches performance mode
+//False - high quality, high demanding, DOM
+//True - lower quality, canvas
+function switchMode() {
+
+    //Array to store paths to playlist js scripts
+    var playlist_arr = [];
+    //Stores state of checkmark(toggle switch)
+    var toggle_switch_status = document.getElementById("toggle_switch").checked;
+    //Gets all the playlist scripts
+    var scripts = document.body.getElementsByClassName("playlist_script");
+    //Get misc script
+    var misc_script = document.getElementById("misc_script")
+
+    //Function for loading every script through promise
+    //src is an argument - "static/{playlist_name}_canvas.js"
+    //We are setting class so we can use those scripts again when toggling the switch
+    function loadScript(src, filter) {
+
+        return new Promise((resolve, reject) => {
+
+            //Creating new script element
+            const script = document.createElement("script");
+            script.setAttribute("src", src);
+            if(filter === "playlist") {
+                script.setAttribute("class", filter+"_script");
+            }
+
+            else if(filter === "misc") {
+                script.setAttribute("id", filter+"_script");
+            }
+
+            script.onload = () => {
+                resolve();
+            }
+
+            script.onerror = () => {
+                reject();
+            }
+
+            document.body.appendChild(script);
+        })
+    }
+
+    //DOM Mode
+    if(!toggle_switch_status) {
+
+        //Clears whole canvas
+        markers_canvas.clear();
+
+        //Adds path to the array and removes script from HTMLCollection
+        for(var i = scripts.length - 1; i >= 0; --i) {
+            playlist_arr.push(scripts[i].getAttributeNode("src").value);
+            scripts[i].remove();
+        }
+        
+        //Remove script with id misc_script
+        misc_script.remove();
+
+        //To keep original order of playlist paths
+        playlist_arr = playlist_arr.reverse();
+
+        //Function to load all the scripts
+        //src is stripped of _canvas so we can use DOM mode scripts
+        async function loadAllScripts(playlist_arr) {
+
+            var playlist_checkboxes = document.querySelectorAll(".playlist_checkbox");
+            var misc_checkboxes = document.querySelectorAll(".misc_checkbox");
+
+            for (let i = 0; i < playlist_arr.length; i++) {
+
+                const src = playlist_arr[i].replace("_canvas", "");
+                await loadScript(src, "playlist");
+
+                //We change onchange function to one that handles DOM mode, see watchCheckbox
+                //We need to send change event so we keep filters when mode is switched
+                var playlist_checkbox = playlist_checkboxes[i].querySelector(".filter_checkbox");
+                playlist_checkbox.attributes.onchange.nodeValue = playlist_checkbox.attributes.onchange.nodeValue.replace("Canvas", "");
+                playlist_checkbox.dispatchEvent(new Event('change'));
+            }
+
+            const src_misc = misc_script.getAttributeNode("src").value.replace("_canvas", "");
+            await loadScript(src_misc, "misc");
+            
+            for (let i = 0; i < misc_checkboxes.length; i++) {
+                var misc_checkbox = misc_checkboxes[i].querySelector(".filter_checkbox");
+                misc_checkbox.attributes.onchange.nodeValue = misc_checkbox.attributes.onchange.nodeValue.replace("Canvas", "");
+                misc_checkbox.dispatchEvent(new Event('change'));
+            }
+        }
+
+        loadAllScripts(playlist_arr);
+    }
+
+    //Canvas mode
+    else if(toggle_switch_status) {
+
+        //Clears whole map pane
+        map.eachLayer((layer)=>{
+            if(layer instanceof L.Marker){
+                layer.remove();
+            }
+        });
+
+        //Adds path to the array and removes script from HTMLCollection
+        for(var i = scripts.length - 1; i >= 0; --i) {
+            playlist_arr.push(scripts[i].getAttributeNode("src").value);
+            scripts[i].remove();
+        }
+
+        //Remove script with id misc_script
+        misc_script.remove();
+
+        //To keep original order of playlist paths
+        playlist_arr = playlist_arr.reverse();
+
+        //Function to load all the scripts
+        //src is stripped of _canvas so we can use DOM mode scripts
+        async function loadAllScripts(playlist_arr) {
+
+            var playlist_checkboxes = document.querySelectorAll(".playlist_checkbox");
+            var misc_checkboxes = document.querySelectorAll(".misc_checkbox");
+
+            for (let i = 0; i < playlist_arr.length; i++) {
+
+                const src = playlist_arr[i].replace(".js", "_canvas.js");
+                await loadScript(src, "playlist");
+
+                //We change onchange function to one that handles DOM mode, see watchCheckbox
+                //We need to send change event so we keep filters when mode is switched
+                var playlist_checkbox = playlist_checkboxes[i].querySelector(".filter_checkbox");
+                playlist_checkbox.attributes.onchange.nodeValue = playlist_checkbox.attributes.onchange.nodeValue.replace("Checkbox", "CanvasCheckbox");
+                playlist_checkbox.dispatchEvent(new Event('change'));
+            }
+
+            const src_misc = misc_script.getAttributeNode("src").value.replace(".js", "_canvas.js");
+            await loadScript(src_misc, "misc");
+            
+            for (let i = 0; i < misc_checkboxes.length; i++) {
+                var misc_checkbox = misc_checkboxes[i].querySelector(".filter_checkbox");
+                misc_checkbox.attributes.onchange.nodeValue = misc_checkbox.attributes.onchange.nodeValue.replace("Checkbox", "CanvasCheckbox");
+                misc_checkbox.dispatchEvent(new Event('change'));
+            }
+        }
+
+        loadAllScripts(playlist_arr);
+
+    }
+}
